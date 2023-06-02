addeab2aa50bc362e0a3eb28438e36cc631f0202
kumar99k <mr.kumar9982@gmail.com>
2023-06-02 03:29:15 -0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+
+<?php include_once("./home.php") ?> 
+
+<div class="img">
+<span class="border border-dark">
+   <img src="./images/dark.webp" class="img-fluid rounded w3-opacity-max" alt="Logo"  style="width:100%; height:700px; ">
+<h1 style="position: absolute;
+            color: white;
+            font-size: 40px;
+            top: 250px;
+            padding: 50px;
+            font-family: 'Titillium Web', sans-serif;">
+    <bold><center>
+        Investo, India's most valuable crypto investment Platform, is dedicated to make crypto accessible in a simple way. Established in 2022, Investo has solving numerous problems faced by the Indian crypto community with solutions around crypto investing, crypto trading & crypto literacy.
+    </center></bold>
+</h1>
+</span>
+</div>
+
+<?php include_once("footer.php") ?>
+
+<!--public_html\FirstPageModules\about.php -->
